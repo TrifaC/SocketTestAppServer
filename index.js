36f500242917f8e7420f0491a8d8eaea84a99d00
Trifa <trifa@hku.hk>
2021-12-15 17:51:39 +0800

    first commit

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
+/************************************** Variable And Constants Declaration *******************************************/
+
+
+// Create Express Service.
 const app = require('express')();
-const http = require('http').Server(app);
+// Bind the HTTP Service.
+const http = require('http').createServer(app);
+// Inject the Socket.io
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
+
+// The object to store the clients.
+let socketObjects = {}
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {
-  socket.on('chat message', msg => {
+
+/************************************** Socket Logic *****************************************************************/
+
+
+// Method will execute during connection event.
+io.on('connection', (client) => {
+  
+  // Append the client into the object, and broadcast that some client has enter the room.
+  // console.log("client connected: The id is " + client.id)
+  socketObjects[client.id] = client
+  io.emit('chat message', "A client " + client.id + " enter the room")
+
+  client.on('chat message', msg => {
+    console.log(msg)
     io.emit('chat message', msg);
+  });
+
+  client.on('disconnect', () => {
+    delete socketObjects[client.id]
+    client.emit('Le')
   });
 });
 
